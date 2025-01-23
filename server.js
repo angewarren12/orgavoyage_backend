39fc611fb5dc30e76362f9d23fc9c330dcb05f27
angewarren <angedesirecamara@gmail.com>
2025-01-23 16:11:43 +0000

    Clean up server.js - remove all reservation references

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // Import des routes
-const reservationRoutes = require('./src/routes/reservationRoutes');
 const autocompleteRoutes = require('./src/routes/autocompleteRoutes');
 const flightRoutes = require('./src/routes/flightRoutes');
 
@@ -15,7 +14,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/reservations', reservationRoutes);
 app.use('/api/autocomplete', autocompleteRoutes);
 app.use('/api/flights', flightRoutes);
 
