011532426e47552bbe199e859ca72382f7df66ac
angewarren <angedesirecamara@gmail.com>
2025-01-23 19:01:52 +0000

    Update CORS configuration to allow requests from Netlify

--- server.js
+++ server.js
@@ -7,16 +7,23 @@ const autocompleteRoutes = require('./src/routes/autocompleteRoutes');
 const flightRoutes = require('./src/routes/flightRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+
+// Configuration CORS
+const corsOptions = {
+  origin: ['https://orgavoyages.netlify.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Routes
 app.use('/api/autocomplete', autocompleteRoutes);
 app.use('/api/flights', flightRoutes);
 
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
